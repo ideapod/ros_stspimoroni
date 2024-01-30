d9174a55d962cac06679f294bf4542aba2a3f65f
Your name <ligalog@icloud.com>
2024-01-30 15:44:51 +1100

    changed setup.py to include copying launch file.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'stspimoroni'
 
@@ -10,12 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mark',
     maintainer_email='ligalog@icloud.com',
-    description='ackage to start up sts pimoroni kit using explorerhat',
+    description='package to start up sts pimoroni kit using explorerhat',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
