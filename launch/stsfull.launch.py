c5b354d8347b2420e18b684d972de17eb75fcd01
Your name <ligalog@icloud.com>
2024-01-30 15:17:36 +1100

    added launch file for transport and camera nodes.

---
+++ launch/stsfull.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='stspimoroni',
+            executable='sts',
+            name='transport'
+        ),
+        Node( 
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            parameters=[
+            {'image_size': '[640,480]'}
+            ],
+            name='camera'
+        )
+    ])
